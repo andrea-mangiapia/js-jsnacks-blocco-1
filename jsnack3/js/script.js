917032b5a2ad4e86c24aff0e3716026c04724811
andrea-mangiapia <andrea.mangiapia@gmail.com>
2022-06-09 16:40:37 +0200

    add jsnack3

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,1 +1,19 @@
 // JSNACK3
+/* 
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+*/
+
+// variabile somma
+let som = 0;
+
+// ciclo di 10 richieste di numeri chiesti all'utente
+for (let i = 0; i < 10; i++) {
+  // inserisco il dato dell'utente nella variabile trasformandolo in numero
+  let userNumber = parseInt(prompt('Scrivi un numero:'));
+  // Eseguo la somma dei numeri ottenuti nel ciclo alla variabile "userNumber"
+  som += userNumber;
+}
+
+// Stampa la somma ottenuta dai numeri inseriti dall'utente.
+console.log(som);
